31813da38ff304ebde519281a2dd00807f2286b1
SergeyShahovski <127224179+SergeyShahovski@users.noreply.github.com>
2023-03-07 18:21:54 +0300

    Delete main_summary_2 directory

--- main_summary_2/script/menu.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('menu_btn').onclick = function() {
-document.getElementById('menu').classList.add('hidden-menu');
-}
-
-
-document.getElementById('menu_btn').onclick = function() {
-document.getElementById('menu').classList.toggle('hidden-menu');
-}
